d8bdcee44651f6d257d86227206ed2417832a68c
Salvador <ssandovalgaribay@gmail.com>
2025-02-17 12:43:26 -0600

    Add files via upload

---
+++ HolaMundoPrueba/src/main/java/com/example/holamundoprueba/HolaMundoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.holamundoprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaMundoPruebaApplication.class, args);
+    }
+
+}
